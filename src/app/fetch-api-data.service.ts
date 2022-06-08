932bea77e425afaa2a82549c6f35c2ecc6a379a3
samisep <sam.sepehri@gmx.de>
2022-06-08 21:06:31 +0200

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://cinesam2022.herokuapp.com/';
+const apiUrl = 'https://samcinema2022.herokuapp.com/';
 
 
 @Injectable({
